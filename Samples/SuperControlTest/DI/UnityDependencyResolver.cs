5d1002ddc882a9eeb1992e7f700f8e4276a6227b
Jose Luis Vaquero Cuevas <jose.vaquero@gpex.es>
2015-11-16 14:51:45 +0100

    Added Unity dependency resolver. Rename NinjectDependencyInjector to NinjectDependencyResolver.

---
+++ Samples/SuperControlTest/DI/UnityDependencyResolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using WebForms.vNextinator;
+
+namespace SuperControlTest.DI
+{
+    /// <summary>
+    ///  Unity dependency resolver. Allows to decouple configuration from Resolver. You can reuse UnityDependentyResolver
+   ///   and configure it in each app without touching it.
+    /// </summary>
+    public sealed class UnityDependencyResolver : IDependencyResolver
+    {
+        private IUnityContainer container;
+
+        private UnityDependencyResolver()
+        {
+            container = new UnityContainer();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return container.Resolve(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return container.ResolveAll(serviceType);
+        }
+
+        public static IDependencyResolver configureAndGet(Action<IUnityContainer> configSteps)
+        {
+            UnityDependencyResolver instance = new UnityDependencyResolver();
+            configSteps(instance.container);
+            return instance;
+
+        }
+    }
+}
